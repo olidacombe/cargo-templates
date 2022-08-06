48306f9eb2abad9839662b45a7c580454aebf248
Oli Dacombe <odacombe@blockchain.com>
2022-08-06 20:31:44 +0100

    point the user at where the README is coming from

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,3 +1,6 @@
+//! README.md is generated from [crate-level
+//! documentation](https://doc.rust-lang.org/rustdoc/how-to-write-documentation.html) by [cargo
+//! readme](https://github.com/livioribeiro/cargo-readme)
 #![deny(missing_docs)]
 
 #[cfg(test)]
