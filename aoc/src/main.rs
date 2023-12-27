f5fced11944c2a820503f7b77650ab07f156b898
Oli Dacombe <oliver.dacombe@gsr.io>
2023-12-28 00:01:49 +0200

    tracing in aoc

--- aoc/src/main.rs
+++ aoc/src/main.rs
@@ -3,6 +3,7 @@ use std::io;
 use aoc{{year}}_{{day}}_{{round}}::{{main-function}};
 
 fn main() {
+    tracing_subscriber::fmt::init();
     let lines = io::stdin().lines().filter_map(|l| l.ok());
     let answer = {{main-function}}(lines);
     println!("Answer: {answer}");
